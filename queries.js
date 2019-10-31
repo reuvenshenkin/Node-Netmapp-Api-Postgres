7090e043f5a6434ffd906474d6f8d3bf563b886f
reuvens <reuven.sheinkin@capriza.com>
2019-10-31 13:10:54 +0200

    initial commit

---
+++ queries.js
@@ -0,0 +1,25 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'gisuser2',
+    host: '212.179.205.15',
+    database: 'gisuser2db',
+    password: 'gisuser#2',
+    port: 5432,
+});
+
+
+const addSignalGPSEntry = (request, response) => {
+    const { Phone_type, MNO, date, signal_strengh_dbm, Latitude, Longitude } = request.body;
+    pool.query('insert into signalgps values ($1, $2, $3, $4, $5, $6)', [Phone_type, MNO, date, signal_strengh_dbm, Latitude, Longitude], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Signal GPS Entry added`);
+    })
+  };
+
+
+
+  module.exports = {
+      addSignalGPSEntry
+  };
